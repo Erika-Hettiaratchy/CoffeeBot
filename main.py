369897f1fe1e1a59f6f5dea01dd0f6ab1c3bc0e4
Erika Hettiaratchy <ehettiaratchy@gmail.com>
2022-10-17 22:39:35 +0900

    Scraping Data and formatting complete.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://www.allrecipes.com/recipes/134/drinks/coffee/")
-soup =  BeautifulSoup(res.text, 'html.parser')
-titles = (soup.select('.card__title'))
-print(soup.select('a'))
+res = requests.get("https://www.allrecipes.com/recipe/235850/starbucks-caramel-frappuccino-copycat-recipe/")
+soup = BeautifulSoup(res.text, 'html.parser')
+#The specific ingredients we want.
+soupspan = soup.select('li.mntl-structured-ingredients__list-item span')
 
 
-"""<span data-ingredient-name="true">ice</span>"""
+#loop to remove <span>
+i = 0
+for s in soupspan:
+  decoded = (s.decode_contents())
+  if i <= 2:
+    print(decoded, end = " ")
+    i += 1
+  if i == 3:
+    i = 0
+    print(",")
